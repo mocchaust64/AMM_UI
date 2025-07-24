0b43c69d0c0ac98eb15e053b52add4e6078be7df
mocchaust64 <mocchaust64@gmail.com>
2025-07-24 16:51:46 +0700

    clean

--- app/tokens/create/page.tsx
+++ app/tokens/create/page.tsx
@@ -132,7 +132,6 @@ export default function CreateToken() {
           image: file,
           imageUrl,
         }))
-        toast.success('Image uploaded successfully')
       } else {
         toast.error('Failed to upload image')
       }
